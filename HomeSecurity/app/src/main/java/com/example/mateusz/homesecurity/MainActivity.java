1d08842977070df3699f7ede731dad65e595495e
matszal <mszalkowski2011@gmail.com>
2018-03-17 20:13:10 +0000

    Added JSON reader for security and background task for efficiency

--- HomeSecurity/app/src/main/java/com/example/mateusz/homesecurity/MainActivity.java
+++ HomeSecurity/app/src/main/java/com/example/mateusz/homesecurity/MainActivity.java
@@ -2,23 +2,28 @@ package com.example.mateusz.homesecurity;
 
 import android.Manifest;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
+
+import com.amazonaws.regions.Regions;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.OnNeverAskAgain;
@@ -28,15 +33,25 @@ import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
 public class MainActivity extends AppCompatActivity {
+    static final String LOG_TAG = MainActivity.class.getCanonicalName();
 
-    private static String CUSTOMER_SPECIFIC_ENDPOINT1;
-    private static String CUSTOMER_SPECIFIC_ENDPOINT2;
-    private static String CUSTOMER_SPECIFIC_ENDPOINT3;
+    //Toast.makeText(this, String.valueOf(sdcard), Toast.LENGTH_SHORT).show();
+
+    private static String CUSTOMER_SPECIFIC_ENDPOINT;       //cse
+    private static String COGNITO_POOL_ID;                  //cp_id
+    private static String AWS_IOT_POLICY_NAME;              //policy
+    private static Regions MY_REGION = Regions.EU_WEST_1;
+    private static String KEYSTORE_NAME;                    //key_name
+    private static String KEYSTORE_PASSWORD;                //key_pass;
+    private static String CERTIFICATE_ID;                   //cert_id
+
+    private String fileName = "/credentials_app.json";
 
     TextView keysWindow;
     Button btnRead;
+    Button btn2;
 
-    File sdcard = new File(Environment.getExternalStorageDirectory()+ "/key.txt");
+    File sdcard = new File(Environment.getExternalStorageDirectory() + fileName);
 
 
     @Override
@@ -49,40 +64,59 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "App started succesfully!",
                 Toast.LENGTH_SHORT).show();
 
-        Log.i ("info", "Done creating the app");
-        Toast.makeText(this, String.valueOf(sdcard), Toast.LENGTH_SHORT).show();
+        Log.i("info", "Done creating the app");
+
 
         keysWindow = (TextView) findViewById(R.id.keysViev);
 
-        btnRead = (Button)findViewById(R.id.readKeysBtn);
-        btnRead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                MainActivityPermissionsDispatcher.readStorageWithPermissionCheck(temp);
 
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                readStorage();
             }
         });
+        t1.start();
+
 
     }
 
     @NeedsPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
     void readStorage() {
-        StringBuilder sb = new StringBuilder();
 
-        //Call this from onCreate as a new thread%%%%%%%%%%%%%%%%%%%%%to be implemented as next...
         try {
-            BufferedReader br = new BufferedReader(new FileReader(sdcard));
-            String line;
+            FileInputStream fis = new FileInputStream((sdcard));
+            String jsonStr = null;
 
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-                sb.append('\n');
+            try{
+                FileChannel fc = fis.getChannel();
+                MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+                jsonStr = Charset.defaultCharset().decode(bb).toString();
+            }catch(Exception e){
+                Toast.makeText(this, "exception after reading json object!", Toast.LENGTH_SHORT).show();
             }
-            br.close();
+            finally {
+                fis.close();
+            }
+
+            JSONObject jsonObj = new JSONObject(jsonStr);
+            JSONObject data = jsonObj.getJSONObject("credentials");
+
+            CUSTOMER_SPECIFIC_ENDPOINT = data.getString("cse");
+            COGNITO_POOL_ID = data.getString("cp_id");
+            AWS_IOT_POLICY_NAME = data.getString("policy");
+            KEYSTORE_NAME = data.getString("key_name");
+            KEYSTORE_PASSWORD = data.getString("key_pass");
+            CERTIFICATE_ID = data.getString("cert_id");
+            keysWindow.setText(CUSTOMER_SPECIFIC_ENDPOINT);
+
         } catch (IOException e) {
             Toast.makeText(getApplicationContext(), "Error While reading file", Toast.LENGTH_SHORT).show();
+        } catch (JSONException e){
+            Toast.makeText(getApplicationContext(), "JSON exception", Toast.LENGTH_SHORT).show();
         }
-        keysWindow.setText(sb);
+
     }
 
     @Override
